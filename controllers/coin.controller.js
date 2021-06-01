4c532265bec19245f1ea6d2650cb99bda54a10c2
HungNhOnPV <anhhungx9propro@gmail.com>
2021-06-01 15:57:00 +0700

    fix bug

--- controllers/coin.controller.js
+++ controllers/coin.controller.js
@@ -67,13 +67,7 @@ module.exports.createHolders = async (req, res, next) => {
           const browser = await puppeteer.launch({
             headless: true,
             args: [
-              '--disable-gpu',
-              '--disable-dev-shm-usage',
-              '--disable-setuid-sandbox',
-              '--no-first-run',
-              '--no-sandbox',
-              '--no-zygote',
-              '--single-process',
+              '--no-sandbox'
             ]
           });
 
